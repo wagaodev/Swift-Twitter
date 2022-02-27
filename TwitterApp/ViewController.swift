8ef93b65a8af85a0bdbabe7864f3dc1d22afc41d
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-02-27 01:02:19 -0300

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by WAGNER GOULART on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
