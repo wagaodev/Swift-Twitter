63555fbf4509b590d3ff1077d7ea61ca6bbb57f8
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-03-19 16:52:14 -0300

    feat: we created a view caption text to insert all codes in and offer to a component

--- TwitterApp/Controller/CaptionTextView.swift
+++ TwitterApp/Controller/CaptionTextView.swift
@@ -9,6 +9,7 @@ class CaptionTextView: UITextView {
     let label = UILabel()
     label.font = UIFont.systemFont(ofSize: 16)
     label.textColor = .darkGray
+    label.text = "What's happening?"
     return label
   }()
 
@@ -23,13 +24,16 @@ class CaptionTextView: UITextView {
   override init(frame: CGRect, textContainer: NSTextContainer?){
     super.init(frame: frame, textContainer: textContainer)
     
-    backgroundColor = .red
+    backgroundColor = .white
     font = UIFont.systemFont(ofSize: 16)
+    
     isScrollEnabled = false
     heightAnchor.constraint(equalToConstant: 300).isActive = true
     
     addSubview(placeholderLabel)
     placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
   }
   
   required init?(coder: NSCoder) {
@@ -54,6 +58,10 @@ class CaptionTextView: UITextView {
 
 // MARK: - Selectors
 
+  @objc func handleTextInputChange(){
+    placeholderLabel.isHidden = !text.isEmpty
+    
+  }
 
 
 
