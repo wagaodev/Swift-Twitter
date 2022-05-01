ebbe0ed5b7b276f18c2611fe2c6eb89698e94069
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-01 04:29:22 -0300

    feat: filter created. text with color and size change

--- TwitterApp/View/ProfileFilterView.swift
+++ TwitterApp/View/ProfileFilterView.swift
@@ -1,16 +1,19 @@
 
 import UIKit
 
+private var reuseIndentifier = "ProfileFilterCell"
+
 class ProfileFilterView: UIView {
 
   // MARK: - Properties
+
 
   lazy var collectionView: UICollectionView = {
     let layout = UICollectionViewFlowLayout()
     let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
     cv.backgroundColor = .white
-//    cv.delegate = self
-//    cv.dataSource = self
+    cv.delegate = self
+    cv.dataSource = self
     return cv
   }()
 
@@ -18,12 +21,15 @@ class ProfileFilterView: UIView {
 
   override init(frame: CGRect){
     super.init(frame: frame)
-    configureUI()
+    backgroundColor = .white
+
+    collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reuseIndentifier)
+    addSubview(collectionView)
+    collectionView.addConstraintsToFillView(self)
+
   }
 
-  func configureUI(){
-    backgroundColor = .red
-  }
+
 
   required init?(coder: NSCoder){
     fatalError("init(coder:) has not been implemented")
@@ -36,4 +42,32 @@ class ProfileFilterView: UIView {
   // MARK: - Helpers
 
 }
+// MARK: - UICollectionViewDataSource
+
+extension ProfileFilterView: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 3
+  }
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIndentifier, for: indexPath) as! ProfileFilterCell
+    return cell
+  }
+}
+// MARK: - UICollectionViewDelegate
+extension ProfileFilterView: UICollectionViewDelegate {
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: frame.width / 3, height: frame.height)
+  }
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return 0
+  }
+}
+
+
 
