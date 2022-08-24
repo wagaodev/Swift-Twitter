c915e2c7a274d2749bc95c61cdc2059c2e91688c
wcfx <wcfx.dev@gmail.com>
2022-08-24 01:41:29 -0300

    feat: searching correctly by username and fullname

--- TwitterApp/Controller/ExploreController.swift
+++ TwitterApp/Controller/ExploreController.swift
@@ -14,12 +14,19 @@ class ExploreController: UITableViewController {
     //MARK: - Properties
     
     private var users = [User](){
-        didSet {
-            tableView.reloadData()
-        }
+        didSet { tableView.reloadData() }
     }
     
-  
+    private var filteredUsers = [User](){
+        didSet { tableView.reloadData() }
+    }
+    
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -27,6 +34,7 @@ class ExploreController: UITableViewController {
         
         configureUI()
         fetchUsers()
+        configureSearchController()
     }
     
     //MARK: - API
@@ -47,16 +55,38 @@ class ExploreController: UITableViewController {
         tableView.rowHeight = 60
         tableView.separatorStyle = .none
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a user"
+        searchController.searchBar.autocapitalizationType = .none
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
+    
 }
 
 extension ExploreController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.user = user
         return cell
+    }
+}
+
+extension ExploreController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else { return }
+        
+        filteredUsers = users.filter({ $0.username.contains(searchText) ||
+            $0.fullname.contains(searchText)
+        })
     }
 }
